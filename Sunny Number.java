1ce28cf54d658441be02a145a72f297e242270df
G522306 <47375569+G522306@users.noreply.github.com>
2022-11-29 07:41:19 +0530

    Create Sunny Number.java
    Sunny Number in Java
    In this section, we will learn what is sunny numbers and how to create a Java program
    to find the sunny numbers. We will also create a Java program to find all the sunny numbers between the specified range.

---
+++ Sunny Number.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import java.util.Scanner;
+
+public class SunnyNumber {
+
+	public static void main(String[] args) {
+		int n;
+		System.out.print("input number");
+		Scanner cn= new Scanner(System.in);
+		n = cn.nextInt();
+		 int m=n+1;
+		 
+		 int  p=(int) Math.sqrt(m);
+		 
+		if((p-Math.floor(p))==0) {
+			System.out.println("Sunney NUmber");
+			
+		}else {
+			System.out.println("Not Sunney  NUmber");
+		}
+		 
+		
+		System.out.println(p);	
+		
+		
+
+	}
+
+}
